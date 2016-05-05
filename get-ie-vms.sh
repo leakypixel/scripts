c43452fce57f1ff9077e9c06e32ef93f4e51f54c
Craig Fletcher <hi@leakypixel.net>
2016-05-05 11:10:47 +0100

    Update ie vm script, add hex code fix script.

--- get-ie-vms.sh
+++ get-ie-vms.sh
@@ -2,10 +2,10 @@ cd
 mkdir ie-vms
 cd ie-vms
 
-wget -i https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE8_WinXP/Linux/IE8.WinXP.For.LinuxVirtualBox.txt
-wget -i https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE9_Win7/Linux/IE9.Win7.For.LinuxVirtualBox.txt
-wget -i https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE10_Win7/Linux/IE10.Win7.For.LinuxVirtualBox.txt
-wget -i https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE11_Win7/Linux/IE11.Win7.ForLinuxVirtualBox.txt
+wget -i https://az412801.vo.msecnd.net/vhd/VMBuild_20141027/VirtualBox/IE8_WinXP/Linux/IE8.WinXP.For.LinuxVirtualBox.txt
+wget -i https://az412801.vo.msecnd.net/vhd/VMBuild_20141027/VirtualBox/IE9_Win7/Linux/IE9.Win7.For.LinuxVirtualBox.txt
+wget -i https://az412801.vo.msecnd.net/vhd/VMBuild_20141027/VirtualBox/IE10_Win7/Linux/IE10.Win7.For.LinuxVirtualBox.txt
+wget -i https://az412801.vo.msecnd.net/vhd/VMBuild_20141027/VirtualBox/IE11_Win7/Linux/IE11.Win7.ForLinuxVirtualBox.txt
 
 if hash unrar 2>/dev/null; then
   unrar x IE11.Win7.ForLinuxVirtualBox.part1.sfx 
