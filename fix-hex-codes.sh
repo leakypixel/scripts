dd7a2178eafa7774c043ad369afddf75d7f76ea5
Craig Fletcher <hi@leakypixel.net>
2016-06-08 15:51:58 +0100

    Improve hex code fix

--- fix-hex-codes.sh
+++ fix-hex-codes.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-grep -rl --include=*.scss "#[a-fA-F0-9]\{3\}[, ;]" | xargs sed -i "s/#\([a-fA-F0-9]\)\([a-fA-F0-9]\)\([a-fA-F0-9]\)\([,; ]\)/#\1\1\2\2\3\3\4/g"
-grep -rl --include=*.scss "#[a-fA-F0-9]\{6\}[, ;]" | xargs sed -i "s/#[a-fA-F0-9]\{6\}[,; ]/\L&/g"
+grep -rl --include=*.scss "#[a-fA-F0-9]\{3\}[), ;]" | xargs sed -i "s/#\([a-fA-F0-9]\)\([a-fA-F0-9]\)\([a-fA-F0-9]\)\([),; ]\)/#\1\1\2\2\3\3\4/g"
+grep -rl --include=*.scss "#[a-fA-F0-9]\{6\}[), ;]" | xargs sed -i "s/#[a-fA-F0-9]\{6\}[),; ]/\L&/g"
