74881601af14d56be71c56943e796cbd7e5f43fc
Craig Fletcher <hi@leakypixel.net>
2018-04-12 14:31:12 +0100

    Clean up

--- branch-matches-git-flow.sh
+++
@@ -1,1 +0,0 @@
-(git rev-parse --abbrev-ref HEAD | grep -Eq  '^(feature|fix|documentation)/[a-z\-]+$') && exit 0 || exit 1
--- create-github-repo.sh
+++
@@ -1,2 +0,0 @@
-curl -u 'leakypixel' https://api.github.com/user/repos -d '{"name":"${1}"}'
-git remote add origin git@GitHub.com:leakypixel/${1}.git
